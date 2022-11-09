eec3eda496b90d0b0f516c5e7bce897e8dfcc272
Hoàng Vũ <81975442+VKH2801@users.noreply.github.com>
2022-11-09 16:31:20 +0700

    Adding Logo, SearchBar

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,16 +1,22 @@
-import React, {useState} from "react";
-import closeIcon from "../../../assets/icons/closeIcon.png";
-
-
-
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+import LogoHeader from "./LogoHeader";
+import SearchBarHeader from "./SearchBarHeader";
 
 const Header = () => {
-  const [openDialogEmailUs, setOpenDialogEmailUs] = useState('dialog hide')
+  const [openDialogEmailUs, setOpenDialogEmailUs] = useState(false);
+  const [openDialogLogin, setOpenDialogLogin] = useState(false);
+  const [showPassword, setshowPassword] = useState(false);
+
   const openEmailUsDialog = () => {
-    return (
-      setOpenDialogEmailUs('dialog')
-    )
-  }
+    return setOpenDialogEmailUs((current) => !current);
+  };
+  const showPasswordFunction = () => {
+    return setshowPassword((current) => !current);
+  };
+  const openDialogLoginFunction = () => {
+    return setOpenDialogLogin((current) => !current);
+  };
 
   return (
     <header className="header">
@@ -54,60 +60,266 @@ const Header = () => {
           >
             <a
               className="EmailUsWebPartLink"
-              href={openEmailUsDialog}
+              href="#"
+              onClick={openEmailUsDialog}
             >
               Email Us
             </a>
-            <div id='MastheadEmailUsWebPart_ContentContainer'>
-              <div id='MastheadEmailUsDialog' className='dialog show_form hide'>
-                <section id="MastheadEmailUsDialogForm" className="form" role="form">
-                  <header className="section_form">
-                    <h2 className="title">Email Us</h2>
-                    <p className="subTitle">We will reply to your message as soon as possible</p>
-                    <button className="buttonClose" title="close"></button>
-                  </header>
-                  <div className="section_form form-section">
-                    <label className="form-label" for="form-input">From:</label>
-                    <span className="form-input-wrapper">
-                      <input id="form-input" name="form-input" className="form-input" type='text' ></input>
-                    </span>
-                  </div>
-                  <div className="section_form message-section">
-                    <div className="expanding-area active">
-                      <pre>
-                        <span className="message-input-clone">
-                          <br></br>
-                        </span>
-                      </pre>
-                      <textarea id="message-input" className="message-input" placeholder="Let us know what you need."></textarea>
-                    </div>
-                  </div>
-                  <footer className="section_form">
-                    <div className="left-container">
-                      <button className="button-send-feedback">SEND</button>
-                      <button className="link-cancel-feedback">CANCEL</button>
-                      <span className="footer-message-container"></span>
-                    </div>
-                    <div className="right-container">
-                      <span className="email-message">
-                        Customer Service Email: 
-                        <a href="mailto:vukhanhhoang2801@gmail.com">vukhanhhoang2801@gmail.com</a>
+            {openDialogEmailUs === true && (
+              <div id="MastheadEmailUsWebPart_ContentContainer">
+                <div id="MastheadEmailUsDialog" className="dialog show_form">
+                  <section
+                    id="MastheadEmailUsDialogForm"
+                    className="form"
+                    role="form"
+                  >
+                    <header className="section_form">
+                      <h2 className="title">Email Us</h2>
+                      <p className="subTitle">
+                        We will reply to your message as soon as possible
+                      </p>
+                      <button
+                        className="buttonClose"
+                        title="close"
+                        onClick={openEmailUsDialog}
+                      ></button>
+                    </header>
+                    <div className="section_form form-section">
+                      <label className="form-label" for="form-input">
+                        From:
+                      </label>
+                      <span className="form-input-wrapper">
+                        <input
+                          id="form-input"
+                          name="form-input"
+                          className="form-input"
+                          type="text"
+                        ></input>
                       </span>
                     </div>
-                  </footer>
-                </section>
+                    <div className="section_form message-section">
+                      <div className="expanding-area active">
+                        <pre>
+                          <span className="message-input-clone">
+                            <br></br>
+                          </span>
+                        </pre>
+                        <textarea
+                          id="message-input"
+                          className="message-input"
+                          placeholder="Let us know what you need."
+                        ></textarea>
+                      </div>
+                    </div>
+                    <footer className="section_form">
+                      <div className="left-container">
+                        <button className="button-send-feedback">SEND</button>
+                        <button
+                          className="link-cancel-feedback"
+                          onClick={openEmailUsDialog}
+                        >
+                          CANCEL
+                        </button>
+                        <span className="footer-message-container"></span>
+                      </div>
+                      <div className="right-container">
+                        <span className="email-message">
+                          Customer Service Email:
+                          <a href="mailto:vukhanhhoang2801@gmail.com">
+                            vukhanhhoang2801@gmail.com
+                          </a>
+                        </span>
+                      </div>
+                    </footer>
+                  </section>
+                </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="divider"></div>
           <div id="ShellLayout_MastheadLogin_Cntnr" className="link-wrapper">
             <div id="MastheadLoginWebPartCntnr">
-              <div className="Clear"></div>
-              <div id="MastheadLoginInner" className="Clear MastheadLoginInner_Hide container--loginbox"></div>
+              <div className="Clear">
+                <div
+                  className="MastheadLoginUsrCtrlCntnr_Show"
+                  id="MastheadLoginUsrCtrlCntnr"
+                >
+                  <div className="LoginUsrCtrlWebPart_ContentCntnr">
+                    <div
+                      className="LoginUsrCtrlWebPart_AlertCntnr link-wrapper"
+                      id="_CrtePswdAlertCntnr"
+                    >
+                      <a
+                        id="LoginUsrCtrlWebPart_LoginLnk"
+                        className="header--masthead text-login "
+                        onClick={openDialogLoginFunction}
+                      >
+                        Log in
+                      </a>
+                      <svg viewBox="0 0 10 6">
+                        <polygon points="0,0 5,6 10,0"></polygon>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {openDialogLogin === true && (
+                <div
+                  id="MastheadLoginInner"
+                  className="Clear MastheadLoginInner_Hide container--loginbox"
+                >
+                  <div id="MastheadLoginInner_Content">
+                    <div id="MastheadLogin">
+                      <div>
+                        <button
+                          className="buttonCloseLogIn"
+                          title="close"
+                          onClick={openDialogLoginFunction}
+                        ></button>
+                        <div>
+                          <div className="VertSpace_space"></div>
+                          <div className="VertSpace_space"></div>
+                        </div>
+                        <div className="LoginDropDown_inset">
+                          <div className="App">
+                            <form>
+                              <div className="CreateLoginFrame_inset">
+                                <h2 className="CreateLoginFrame_frameHeader">
+                                  Log in
+                                </h2>
+                                <div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                </div>
+                                <div>
+                                  <div className="Input_inputCover Input_neutral undefined">
+                                    <div>
+                                      <input
+                                        spellCheck="false"
+                                        type="text"
+                                        required
+                                        id="Email"
+                                        className="InputField_input InputField_fullwidth"
+                                      ></input>
+                                    </div>
+                                    <label className="Input_labelFloat">
+                                      Email
+                                    </label>
+                                  </div>
+                                </div>
+                                <div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                </div>
+                                <div className="Input_inputCover Input_neutral undefined">
+                                    {showPassword === true ? (
+                                      <div>
+                                          <div>
+                                            <input
+                                              type="text"
+                                              required
+                                              id="password"
+                                              className="InputField_input InputField_buttonwidth"
+                                            ></input>
+                                          </div>
+                                          <div className="ShowHideInput_container">
+                                          <button
+                                            className="ShowHideInput_button"
+                                            type="button"
+                                            onClick={showPasswordFunction}
+                                          >
+                                            hide
+                                          </button>
+                                        </div>
+                                      </div>
+                                    ) : (
+                                      <div>
+                                          <div>
+                                            <input
+                                              type="password"
+                                              required
+                                              id="password"
+                                              className="InputField_input InputField_buttonwidth"
+                                            ></input>
+                                          </div>
+                                          <div className="ShowHideInput_container">
+                                          <button
+                                            className="ShowHideInput_button"
+                                            type="button"
+                                            onClick={showPasswordFunction}
+                                          >
+                                            show
+                                          </button>
+                                        </div>
+                                      </div>
+                                    )}
+                                  
+                                  <label className="Input_labelFloat">
+                                    Password
+                                  </label>
+                                </div>
+                                <div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                </div>
+                                <div className="Dyad_base">
+                                  <div className="Dyad_left">
+                                    <div>
+                                      <input
+                                        className="ValidatedCheckboxInput_customBox"
+                                        type="checkbox"
+                                      ></input>
+                                      <label className="ValidatedCheckboxInput_text">
+                                        <span>Stay logged in</span>
+                                      </label>
+                                    </div>
+                                  </div>
+                                  <div className="Dyad_right">
+                                    <button
+                                      type="button"
+                                      className="CreateLoginFrame_link"
+                                    >
+                                      Reset Password
+                                    </button>
+                                  </div>
+                                </div>
+                                <div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                </div>
+                                <button
+                                  type="submit"
+                                  className="FormButton_primaryButton"
+                                >
+                                  Log in
+                                </button>
+                                <div>
+                                  <div className="VertSpace_space"></div>
+                                  <div className="VertSpace_space"></div>
+                                </div>
+                                <button className="FormButton_secondaryButton">
+                                  Create login
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                        <div>
+                          <div className="VertSpace_space"></div>
+                          <div className="VertSpace_space"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-            
           </div>
         </div>
+        <LogoHeader></LogoHeader>
+        <SearchBarHeader></SearchBarHeader>
       </div>
     </header>
   );
---
+++ src/components/organisms/Header/LogoHeader/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LogoHeader = () => {
+  return (
+    <div id="ShellLayout_Logo_Cntnr" className="shell-layout-logo-container">
+      <div className="logo-web-part">
+        <a id="LogoWebPart_Link" href="/">
+          <img
+            src="https://images1.mcmaster.com/init/gfx/MastheadLogo.svg?ver=1545146094"
+            alt="McMaster-Carr logo"
+          ></img>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default LogoHeader;
