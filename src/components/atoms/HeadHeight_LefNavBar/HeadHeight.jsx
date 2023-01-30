f57bf0eaa3749655c6b6288e965f6155e3c3ff60
Hoàng Vũ <81975442+VKH2801@users.noreply.github.com>
2023-01-30 09:59:21 +0700

    adding feature fillter

---
+++ src/components/atoms/HeadHeight_LefNavBar/HeadHeight.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+
+export default function HeadHeight() {
+  return (
+    <div
+      class="SpecSrch_Attribute SpecSrch_AttrShow SpecSrch_AttrExpanded"
+      id="SS_E_416194212751_416194212751"
+    >
+      <table class="SpecSrch_AttrTitleSpacing SpecSrch_AttrTitle_WithIndustrialInfo">
+        <tbody>
+          <tr>
+            <td class="ni"></td>
+            <th class="SpecSrch_Lbl lc">
+              <div class="SpecSrch_AttrTitleAndIconContainer SpecSrch_AttrTitleAndIconContainer_FillRow">
+                <div ii-compare-prodvalids="76687624201,2625868199032,3618791551706">
+                  <div class="AttrNm_Lbl Hide">Thread Direction</div>Thread
+                  Direction
+                </div>
+                <div class="SpecSrch_IndustrialInfoIconAndCaption SpecSrch_AttrHeading_Compare_IndustrialInfoIcon SpecSrch_AttrHeading_Compare_IndustrialInfoIconPadding SpecSrch_Attribute_IndustrialInfoIconAndCaption">
+                  <img
+                    class="SpecSrch_IndustrialInfoIcon"
+                    title="About Thread Direction"
+                    src="https://images1.mcmaster.com/mvC/gfx/IndustrialInfo/industrial-information-icon.svg?ver=1594717164"
+                    data-filterwithidind="False"
+                    width="12"
+                    height="12"
+                    alt="click to learn more about "
+                  />
+                </div>
+              </div>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr>
+            <td class="ni">
+              <table>
+                <tbody>
+                  <tr>
+                    <td
+                      class="mg ng ni SpecSrch_Value"
+                      id="SS_E_416194212751_416194212751_T_76687624201_76687624201"
+                      title=""
+                      style={{width: '163px'}}
+                    >
+                      <div
+                        class="SpecSrch_ValAndIndustrialInfoContainer SpecSrch_ValAndIndustrialInfoContainerHorizontal"
+                        tabindex="0"
+                      >
+                        <div class="SpecSrch_IndustrialInfoTextContainer mp mr">
+                          <a class="od" href="thread-direction~right-hand/">
+                            Right Hand
+                          </a>
+                        </div>
+                        <div class="SpecSrch_IndustrialInfoIconAndCaption mp mr">
+                          <img
+                            class="SpecSrch_IndustrialInfoIcon"
+                            title="About Right Hand Thread Direction"
+                            src="https://images1.mcmaster.com/mvC/gfx/IndustrialInfo/industrial-information-icon.svg?ver=1594717164"
+                            data-filterwithidind="False"
+                            width="12"
+                            height="12"
+                            alt="learn more about Right Hand by pressing accesskey, i."
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td
+                      class="mg ng ni SpecSrch_Value"
+                      id="SS_E_416194212751_416194212751_T_2625868199032_2625868199032"
+                      title=""
+                      style={{width: '163px'}}
+                    >
+                      <div
+                        class="SpecSrch_ValAndIndustrialInfoContainer SpecSrch_ValAndIndustrialInfoContainerHorizontal"
+                        tabindex="0"
+                      >
+                        <div class="SpecSrch_IndustrialInfoTextContainer mp mr">
+                          <a class="od" href="thread-direction~left-hand/">
+                            Left Hand
+                          </a>
+                        </div>
+                        <div class="SpecSrch_IndustrialInfoIconAndCaption mp mr">
+                          <img
+                            class="SpecSrch_IndustrialInfoIcon"
+                            title="About Left Hand Thread Direction"
+                            src="https://images1.mcmaster.com/mvC/gfx/IndustrialInfo/industrial-information-icon.svg?ver=1594717164"
+                            data-filterwithidind="False"
+                            width="12"
+                            height="12"
+                            alt="learn more about Left Hand by pressing accesskey, i."
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td
+                      class="mg ng ni SpecSrch_Value"
+                      id="SS_E_416194212751_416194212751_T_3618791551706_3618791551706"
+                      title=""
+                      style={{width: '163px'}}
+                    >
+                      <div
+                        class="SpecSrch_ValAndIndustrialInfoContainer SpecSrch_ValAndIndustrialInfoContainerHorizontal"
+                        tabindex="0"
+                      >
+                        <div class="SpecSrch_IndustrialInfoTextContainer mp mr">
+                          <a
+                            class="od"
+                            href="thread-direction~right-hand-and-left-hand/"
+                          >
+                            Right Hand and Left Hand
+                          </a>
+                        </div>
+                        <div class="SpecSrch_IndustrialInfoIconAndCaption mp mr">
+                          <img
+                            class="SpecSrch_IndustrialInfoIcon"
+                            title="About Right Hand and Left Hand Thread Direction"
+                            src="https://images1.mcmaster.com/mvC/gfx/IndustrialInfo/industrial-information-icon.svg?ver=1594717164"
+                            data-filterwithidind="False"
+                            width="12"
+                            height="12"
+                            alt="learn more about Right Hand and Left Hand by pressing accesskey, i."
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/components/molecules/ProductPageContent/ProductPageContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ProductPageContent () {
+    return (
+        <div className="jo" id="ProdPageContent" style={{width: '1113px', height: '2291px'}}>
+            <div id="ProdPageContent_Inner">
+                
+            </div>
+        </div>        
+    )
+}
